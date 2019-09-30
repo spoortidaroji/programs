86c1889309f6e747a0d0d0c59beff5a2d67c946e
Spoorti S Daroji <43513480+spoortidaroji@users.noreply.github.com>
2019-09-30 04:18:25 -0700

    Add files via upload

---
+++ hamittonian and lagrangian.c
@@ -0,0 +1,23 @@
+﻿void main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i,j=0,m=n-1;
+    for(i=0;i<n;i++)
+    {
+     scanf("%d",&a[i]);   
+    }
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if(a[i]>=a[j]){
+                continue;}
+                else
+                break;
+            }
+            if(j==n)
+            printf("%ld ",a[i]);
+           
+            else
+            continue;
+        } 
+    }
